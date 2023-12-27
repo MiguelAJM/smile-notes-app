1447cc0795b6224761c97d04d2454cbb7e0ef886
Miguel Angel Jimenez <koekun3000@gmail.com>
2023-12-26 20:36:16 -0500

    #UPDATE: Firebase Config

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  apiKey: 'AIzaSyDLak7wXcv74UhFdgOPptgh3T04nTZyB64',
+  authDomain: 'smile-notes.firebaseapp.com',
+  projectId: 'smile-notes',
+  storageBucket: 'smile-notes.appspot.com',
+  messagingSenderId: '706657334795',
+  appId: '1:706657334795:web:e82b8afa35135e3673390e',
+  measurementId: 'G-ZBRXJLM8D9'
 }
 
 // Initialize Firebase
