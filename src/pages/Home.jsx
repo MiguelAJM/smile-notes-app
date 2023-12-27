3ee07d7ff310031548ba873d2afc6cb8ec9effdf
Miguel Angel Jimenez <koekun3000@gmail.com>
2023-12-26 20:30:59 -0500

    #ADD: Vercel options

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,15 @@ import Layout from '../components/Layout'
 export default function Home() {
   return (
     <Layout>
-      <div>Resumen...</div>
+      <div className='grid grid-cols-3 gap-4'>
+        <div className='aspect-square bg-black rounded-2xl'>
+          Tarea completadas: 0
+        </div>
+        <div className='aspect-square bg-black rounded-2xl'>
+        </div>
+        <div className='aspect-square bg-black rounded-2xl'></div>
+      </div>
+
     </Layout>
   )
 }
