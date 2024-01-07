bf18f25ee04deadf9a47387eca024b528f14d801
Miguel Angel Jimenez <koekun3000@gmail.com>
2024-01-07 02:20:35 -0500

    #FIX: Error in PrivateRoutes

--- src/components/PrivateRoutes.jsx
+++ src/components/PrivateRoutes.jsx
@@ -3,5 +3,5 @@ import { useAuth } from '../context/AuthProvider'
 
 export default function PrivateRoutes({ children }) {
   const { user } = useAuth()
-  return user !== undefined ? children : <Navigate replace to='/login' />
+  return user !== null ? children : <Navigate replace to='/login' />
 }
